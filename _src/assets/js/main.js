b95c49f6df1af74e3264c7c37ae1a6c47e252dc2
Elvira <elvi_ac94@hotmail.com>
2020-03-20 09:28:09 +0100

    add function to show series list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,28 @@
 'use strict';
+const inputName = document.querySelector('#main__input');
+const searchButton = document.querySelector('#button-search');
+const ulList = document.querySelector('.main__list');
+const URL = 'http://api.tvmaze.com/search/shows?q='
 
-console.log('>> Ready :)');
+function getSerieName(){
+
+    console.log(inputName.value)
+    fetch(`http://api.tvmaze.com/search/shows?q=${inputName.value}`)
+    .then(response => response.json())
+    .then(data =>{
+        const seriesTV = data
+        console.log(seriesTV)
+        getSerieList(seriesTV)
+        
+     })
+}
+
+function getSerieList(seriesTV){
+    let list= ``;
+        for( const serie of seriesTV){
+            list +=`<li><p>${serie.show.name}</p><img src=${serie.show.image.medium}>`
+        }
+        ulList.innerHTML=list
+}
+
+searchButton.addEventListener('click',getSerieName);
